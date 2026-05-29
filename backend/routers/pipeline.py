b98bd97d785123aa989c732cd2909cc5586fed6c
Krunal <nsundersia@gmail.com>
2026-05-29 10:57:57 +0530

    Update pipeline.py

--- backend/routers/pipeline.py
+++ backend/routers/pipeline.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, BackgroundTasks, HTTPException
 from backend.services.data_service import invalidate_cache
 import subprocess
 import sys
-#00s0jhfkun
+#00s0jhfku
 router = APIRouter()
 
 pipeline_status = {
